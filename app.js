d280926b33f74041a0ac26e42ec84ab24a43468c
vargascristianj <vargascristianj@gmail.com>
2025-01-29 18:02:30 -0300

    Agrego ajustes de logica, html y estilos

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ let posicionRandom = 0;
 function agregarAmigo() {
     let nombreingresado = document.getElementById('amigo').value;
     if(nombreingresado.trim().length != 0){ //No se permite el ingreso de nombres vacíos o espacios solamente
-        console.log('Nombre ingresado: ' +nombreingresado);
         amigos.push(nombreingresado);
-        console.log('Arreglo: ' +amigos);
     }else{
         alert('Por favor, ingrese un nombre');
-        console.log('Ingreso vacío');        
+    }
+    if(amigos.length >= 2){ // Valido que ingrese al menos 2 nombres para habilitar el botón sortear.
+       document.getElementById('button-sortear').removeAttribute('disabled');
+       document.getElementById('texto-info').style.display = 'none';
+       asignarTextoElemento('texto-info',''); //Quito el texto informativo
     }
     limpiarCaja	();
     listarAmigos(amigos);
+    document.getElementById('amigo').focus(); //vuelvo el cursor al cuadro de texto
 }
 
 //Función para limpiar la caja del nombre
@@ -29,7 +32,6 @@ function listarAmigos(arreglo) {
    asignarTextoElemento('listaAmigos',"");
    let listaNombres = "";
    for (let i = 0; i < arreglo.length; i++){
-    console.log(`Nombre #${i}: ${arreglo[i]}`);
     listaNombres += `<li>${arreglo[i]}</li>`;
     asignarTextoElemento('listaAmigos', listaNombres);
    }
@@ -41,26 +43,39 @@ function sortearAmigo() {
     let cantidad = amigos.length;
     if(cantidad != 0){
     posicionRandom = Math.floor(Math.random()*cantidad);
-    console.log("Amigo secreto: "+amigos[posicionRandom]);
     asignarTextoElemento('listaAmigos',"");
     asignarTextoElemento('resultado',`Tu amigo secreto es ${amigos[posicionRandom]}`) 
-    document.getElementById('button-sortear').setAttribute('disabled', 'true');
+    document.getElementById('button-sortear').style.visibility = 'hidden';
+    document.getElementById('button-add').setAttribute('disabled', 'true');
+    document.getElementById('button-reiniciar').style.visibility = 'visible';
     } else {
         alert("Aún no se han ingresado amigos");
     }
     return;
 }
 
+//Función para modificar el texto en el HTML
 function asignarTextoElemento(elementoId, texto) {
     let elementoHTML = document.getElementById(elementoId);
     elementoHTML.innerHTML = texto;    
 }
 
+//Función para establecer las variables iniciales
+function setearCondicionesIniciales() {
+    amigos = [];
+    posicionRandom = 0;
+    document.getElementById('button-add').removeAttribute('disabled');
+    document.getElementById('button-sortear').style.visibility = 'visible';
+    document.getElementById('button-sortear').setAttribute('disabled', 'true');
+    document.getElementById('button-reiniciar').style.visibility = 'hidden';
+    asignarTextoElemento('listaAmigos',"");
+    asignarTextoElemento('resultado',"");
+    document.getElementById('texto-info').style.display = 'block';
+    asignarTextoElemento('texto-info','Info: Ingrese al menos dos amigos');
+}
+
+setearCondicionesIniciales();
+
 /* PENDIENTES: 
-1. Posicionar el cursor en el cuadro de texto luego de ingresar un nombre. Ver si es posible con "Enter".
 2. Hacer responsivo el HTML.
-3. Cambiar el color al deshabilitar el botón.
-4. Poner botón reiniciar. Se muestra o habilita luego del ingreso de un nombre o de un sorteo.
-5. Validar una cantidad mínima de 2 nombres para permitir el sorteo.
-6. Quitar console logs.
 */
